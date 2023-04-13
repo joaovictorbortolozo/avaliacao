be7b15aec2a02c7b076897dbab23aa68700fc5fb
João Victor Bortolozo <110576032+joaovictorbortolozo@users.noreply.github.com>
2023-04-12 21:50:57 -0300

    avaliacao

--- exercicio4.php
+++ exercicio4.php
@@ -1,18 +1,33 @@
 Usando PHP você deve fazer um programa que acenda e apague a lâmpada
 <br>
-<select name="">
-    <option value="apagado.png">Apagado</option>
-    <option value="acesso.png">Aceso</option>
-</select>
+<html>
+<head>
+	<title>Exibir Imagem com PHP</title>
+</head>
+<body>
+	<form method="post">
+		<select name="imagem">
+			<option value="acesa.png">acesa</option>
+			<option value="apagada.png">apagada</option>
+		</select>
+		<input type="submit" name="exibir" value="Exibir">
+	</form>
 
-<?php
-
-if ($apagado){
-   echo "$img = imagecreatefromjpeg('/imagens/acesa.jpg')";
-   readfile('aceso.png');
-}
-
-
-
-
-?>
+	<?php
+		if (isset($_POST['exibir'])) {
+			$imagemSelecionada = $_POST['imagem'];
+			switch ($imagemSelecionada) {
+				case 'acesa.png':
+					echo '<img src="acesa.png">';
+					break;
+				case 'apagada.png':
+					echo '<img src="apagada.png">';
+					break;
+				default:
+					echo 'Selecione uma imagem para exibir';
+					break;
+			}
+		}
+	?>
+</body>
+</html>
